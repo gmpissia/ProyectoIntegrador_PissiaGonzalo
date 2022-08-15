951c238f9a68cdb6a1030d978b81877d600e835b
gmpissia <gmpissia@gmail.com>
2022-08-15 17:55:24 -0300

    Back End iniciado con sus dependencias

---
+++ ProyectoIntegrador_GonzaloPissia/BackEnd/gmp/src/main/java/com/portfolio/gmp/GmpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmpApplication.class, args);
+	}
+
+}
